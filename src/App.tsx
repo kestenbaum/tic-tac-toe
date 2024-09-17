d679dedf51baf97eb28f7db6def5876ac2aa1e88
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-17 14:33:22 +0200

    create player

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import {useState} from "react";
+
+interface EditingName {
+  edit: boolean
+}
+
 export default function App() {
+  const [isEdit, setIsEdit]:EditingName = useState(false)
+
   return (
     <>
-     init
+        <span>
+          Player
+          <span>Name Player 1</span>
+          <span>Symbol Player Symbol</span>
+        </span>
     </>
   )
 }
