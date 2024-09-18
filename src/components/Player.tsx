ecf0c2a2e3fcbf5ff9d909af06ddb21caf40c0aa
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-19 00:28:23 +0200

    update component Player: fix jsx and styles[name and symbol]

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -3,16 +3,13 @@ import {PlayerInterface} from "../../@types";
 
 const Player :FC<PlayerInterface> = ({name, symbol}) => {
     return (
-        <div className="player">
-            <div className="wrapper__name">
-                <span>Player: </span>
-                <span className="player__name">{name}</span>
-            </div>
-            <div className="wrapper__name">
-                <span>Player Symbol: </span>
-                <span className="player__name">{symbol}</span>
-            </div>
-        </div>
+        <>
+            <li className="player">
+                <span className="player-name">Player: {name}</span>
+                <span className="player-symbol">Player Symbol: {symbol}</span>
+            </li>
+            <button>Edit</button>
+        </>
     );
 };
 
