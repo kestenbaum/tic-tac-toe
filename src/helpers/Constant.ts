fb68a36538bfb680cbe0a9491e70bd191f4e5322
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-21 00:14:47 +0200

    add file Constant

---
+++ src/helpers/Constant.ts
@@ -0,0 +1,10 @@
+export const INITIAL_BOARD = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+export const INITIAL_PLAYERS:initialPlayers = {
+    "X": "Player 1",
+    "O": "Player 2"
+}
