602c6507fd098eccf565023b59d087385af85738
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-21 00:15:23 +0200

    add component Players

---
+++ src/components/Players.tsx
@@ -0,0 +1,13 @@
+import Player from "./Player.tsx";
+import {INITIAL_PLAYERS} from "../helpers/Constant.ts";
+
+const Players = () => {
+    return (
+        <ul id={"players"}>
+            <Player name={INITIAL_PLAYERS.X} symbol={"X"}/>
+            <Player name={INITIAL_PLAYERS.O} symbol={"O"}/>
+        </ul>
+    );
+};
+
+export default Players;
