76ceacccaeed0a4a42335d0bc03ccac263d452ea
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-21 00:16:50 +0200

    update component Player: add style active Player

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,12 @@
 import {ChangeEvent, FC, useState} from "react";
 
-const Player:FC<PlayerInterface> = ({name, symbol}) => {
+const Player:FC<PlayerInterface> = ({name, symbol, isActive}) => {
     const [isEdit, setIsEdit] = useState<boolean>(false);
     const [namePlayer, setNamePlayer]  = useState<string>(name);
 
     const playerName = !isEdit
-        ? <span className="player-name">Player: {namePlayer}</span>
-        : <input className={"input"} onChange={handleChangeName} value={namePlayer}/>
+        ? <span className="player-option">Player: {namePlayer}</span>
+        : <input className="input" onChange={handleChangeName} value={namePlayer}/>
 
     function handleChangeName (event: ChangeEvent<HTMLInputElement>) {
             setNamePlayer(event.target.value)
@@ -17,13 +17,13 @@ const Player:FC<PlayerInterface> = ({name, symbol}) => {
     }
 
     return (
-        <>
-            <li id="player">
+        <li className={isActive ? "player-active" : undefined}>
+            <div className="player-wrapper">
                 {playerName}
-                <span className="player-symbol">Player Symbol: {symbol}</span>
-            </li>
+                <span className="player-option">Player Symbol: {symbol}</span>
+            </div>
             <button onClick={handleEditClick}>Edit</button>
-        </>
+        </li>
     );
 };
 
