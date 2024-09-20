5bf2667590137033550fcabd57748929085d3b9a
Oleksii Vovnenko <kespa71for@gmail.com>
2024-09-20 20:43:17 +0200

    refactor component App:

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import Container from "./components/Container.tsx";
 
 function App() {
   return (
-    <>
        <Container>
            <ul id={"players"}>
                <Player name={"Alex"} symbol={"x"}/>
                <Player name={"Nick"} symbol={"0"}/>
            </ul>
        </Container>
-    </>
   )
 }
 
